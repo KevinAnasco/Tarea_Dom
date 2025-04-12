875fe5f6473b754fbf5f6484818920ce2a83e355
KevinAnasco <144829386+KevinAnasco@users.noreply.github.com>
2025-04-11 21:00:35 -0500

    Funcion Agregar Tarea
    Script Funcion Agregar Tarea

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function actualizarContador() {
     const pendientes = tareas.filter(t => !t.completada).length;
     document.getElementById('contador-tareas').textContent = `Tareas pendientes: ${pendientes}`;
 }
+
+function agregarTarea() {
+    const inputTarea = document.getElementById('titulo-tarea');
+    const titulo = inputTarea.value.trim();
+    const inputFecha = document.getElementById('fecha-tarea');
+    const fecha = inputFecha.value;
+
+    if (titulo === '') {
+        alert('Por favor, escribe una tarea.');
+        return;
+    }
+
+    tareas.push({ titulo, fecha, completada: false });
+    guardarTareasEnLocalStorage();
+    inputTarea.value = '';
+    inputFecha.value = '';
+    renderizarTareas();
+}
